0e2863e3e500f1f2137efcc4e397a83a9b3dc607
dk <kudin.dima@gmail.com>
2026-05-02 18:26:16 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Fusion Grill | Luxury Fine Dining | Chef-Driven Culinary Experience',
+  description: 'Experience Fusion Grill, where local ingredients meet chef-driven excellence. Indulge in our signature dishes or host your private event with us.',
+  openGraph: {
+    "title": "Fusion Grill | Luxury Fine Dining",
+    "description": "Experience Fusion Grill, where local ingredients meet chef-driven excellence.",
+    "siteName": "Fusion Grill",
+    "type": "website"
+  },
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
